0cba6e9d31b344f16b301cdb0e2606e60a6a0b39
Solgrey <sol.medina@duocuc.cl>
2026-05-06 00:15:27 -0400

    Se agregan micorservicios de manera pareja

---
+++ backend/ms-flujo-urgencias/src/main/java/cl/rednorte/ms_flujo_urgencias/MsFlujoUrgenciasApplication.java
@@ -0,0 +1,13 @@
+package cl.rednorte.ms_flujo_urgencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFlujoUrgenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFlujoUrgenciasApplication.class, args);
+	}
+
+}
